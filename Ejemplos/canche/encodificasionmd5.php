57b647583ace4ac4698a70a0524afff674490e36
davidcanchejimunav <david_14516@hotmail.com>
2014-03-19 20:51:12 -0700

    encodificasion

---
+++ Ejemplos/canche/encodificasionmd5.php
@@ -0,0 +1,15 @@
+<?php
+
+// definimos las variables y asignamos los valores
+   $password = holacompañero;
+       // MD5   definimos el algorimo a utilizar para que no se muestre  las contraseñas
+	   
+	   $k = md5 ($password);
+	     
+		 // muestra el resultado incriptado
+		 
+		        echo $k;
+	    
+	   
+
+?>
