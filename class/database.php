ba3ac42e11b546808f423857f9fdd64059652dbf
Daniel Lozano Carrillo <daniel@unav.edu.mx>
2014-03-27 09:07:47 -0700

    Funciones a database.class
    :)! selectAll, selectField, insertValues

--- class/database.php
+++ class/database.php
@@ -7,6 +7,7 @@
  */
 
 // incluir archivo con todas las variables necesarias
+
 include("constants.php");
       
 class MySQLDB//Inicio de clase
@@ -28,19 +29,59 @@ class MySQLDB//Inicio de clase
     $result = mysql_query($query, $this->connection);
 //    $dbarray = mysql_fetch_array($result);
     return $result;
-
-   
    }
-
-   function getUser($user){
-   	global $database;
-    $query=("SELECT * FROM blog_users WHERE username = '".$user."'");
-    $result = mysql_query($query, $this->connection);
+   //funcion para seleccionar toda la informacion de una tabla, regresa un array con toda la informacion
+   function selectAll($table){
+    global $database;
+    //echo $query;
+    $q=("SELECT * FROM ".$table."");
+    $result = mysql_query($q, $this->connection);
     $dbarray = mysql_fetch_array($result);
     return $dbarray;
+   }
+   // Esta funcion selecciona la informacion de un campo especifico. regresa un array
+   function selectField($table,$field,$id){
+    global $database;
+    //echo $query;
+    $q=("SELECT * FROM".$table." WHERE ".$field." = '".$id."'");
+    $result = mysql_query($q, $this->connection);
+    $dbarray = mysql_fetch_array($result);
+    return $dbarray;
+   }
+   
+
+
+   //Esta funcion borra un campo especificandole la tabla el nombre del campo y  el identificador.
+   function deleteField($table,$field_name,$id){
+    global $database;
+    $query=("DELETE FROM ".$table."WHERE ".$field_name." = '".$id."'");
+    $result = mysql_query($query, $this->connection);
+    return $result;
+   }
 
    
+   function updateField($table,$field,$value,$field2,$id){
+      $q = "UPDATE ".$table." SET ".$field." = '$value' WHERE $field2 = '$id'";
+      return mysql_query($q, $this->connection);
    }
+
+
+   function insertValues($table,$values){
+
+    //"INSERT INTO ".TBL_USERS." VALUES ('$username', '$password', '0', $ulevel, '$email', $time)"
+      $q = "INSERT INTO".$table."VALUES".$values."";
+      return mysql_query($q, $this->connection);
+   }
+
+
+
+   }
+
+
+
+
+   
+
 };
 
 /* Crear conexion a base de datos */
